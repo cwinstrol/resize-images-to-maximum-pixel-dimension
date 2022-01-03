c335c17cec8b52ea466e802538a90225ed63968d
J. Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2022-01-03 10:58:31 -0500

    Update resize-images-to-maximum-pixel-dimension.py

--- resize-images-to-maximum-pixel-dimension.py
+++ resize-images-to-maximum-pixel-dimension.py
@@ -1,1 +1,19 @@
-
+#dn=''
+def imgs_resize(dn):
+    for p,ds,fs in os.walk(dn):
+        p=os.path.join(p,'')
+        if p.endswith('ir\\'):
+            for f in fs:
+                with Image.open(os.path.join(p+f)) as image:
+                    w,h=image.size
+                    if w>1440 or h>1440:
+                        if (w>h):
+                            nw=1440
+                            nh=int(h/w*1440)
+                            image=image.resize((nw,nh),Image.ANTIALIAS)
+                        else:
+                            nw=int(w/h*1440)
+                            nh=1440
+                            image=image.resize((nw,nh),Image.ANTIALIAS)
+                    image=ImageOps.exif_transpose(image)
+                    image.save(p[:-3]+f,quality=40,optimize=True)
